0997b88ffa1cc50e4b4b18a8c874ba05f1572455
ImranSerifovic <116844006+ImranSerifovic@users.noreply.github.com>
2024-04-14 02:31:27 -0400

    yep

--- src/autons.cpp
+++ src/autons.cpp
@@ -6,23 +6,25 @@ void default_constants(){
   // Constant form: (maxVoltage, kP, kI, kD, startI).
 
 
-  chassis.set_drive_constants(10, 1.5, 0, 10, 0);
-
+  chassis.set_drive_constants(12, 1.2, 0, 0, 0);
+  chassis.set_turn_constants(12, .1, 0, 0, 0);
 
   chassis.set_heading_constants(6, .4, 0, 1, 0);
-  chassis.set_turn_constants(12, .4, .03, 3, 15);
+  
   chassis.set_swing_constants(12, .3, .001, 2, 15);
 
   // Exit condition form: (settle_error, settle_time, timeout).
-  chassis.set_drive_exit_conditions(1.5, 300, 5000);
-  chassis.set_turn_exit_conditions(1, 300, 3000);
+  chassis.set_drive_exit_conditions(1.5, 150, 1000);
+  chassis.set_turn_exit_conditions(1, 500, 3000);
   chassis.set_swing_exit_conditions(1, 300, 3000);
 }
 /*---------------------------------------------------------------------------*/
 
 // PID Test
 void pidTest() { 
-  chassis.drive_distance(16);
+  chassis.turn_to_angle(180);
+  Controller1.Screen.print(Inertial1.angle());
+
 }
 
 
@@ -46,7 +48,7 @@ void FarSideSafe() {
   chassis.drive_distance(5);
   // Back up 
   chassis.drive_distance(-43);
-  // Drive in curve along the match load bar
+  // Drive in curve along the match load ba
 }
 /*---------------------------------------------------------------------------*/  
 /*                            CLOSE SIDE SAFE                                */
--- src/main.cpp
+++ src/main.cpp
@@ -29,30 +29,75 @@ competition Competition;
 /*                                                                           */
 /*---------------------------------------------------------------------------*/
 Drive chassis(
-ZERO_TRACKER_NO_ODOM,
+
+//Specify your drive setup below. There are eight options:
+//ZERO_TRACKER_NO_ODOM, ZERO_TRACKER_ODOM, TANK_ONE_ENCODER, TANK_ONE_ROTATION, TANK_TWO_ENCODER, TANK_TWO_ROTATION, HOLONOMIC_TWO_ENCODER, and HOLONOMIC_TWO_ROTATION
+//For example, if you are not using odometry, put ZERO_TRACKER_NO_ODOM below:
+ ZERO_TRACKER_NO_ODOM,
+//TANK_TWO_ROTATION, // skillschange
+//Add the names of your Drive motors into the motor groups below, separated by commas, i.e. motor_group(Motor1,Motor2,Motor3).
+//You will input whatever motor names you chose when you configured your robot using the sidebar configurer, they don't have to be "Motor1" and "Motor2".
+
 //Left Motors:
 motor_group(Left1, Left2, Left3, Left4),
+
 //Right Motors:
 motor_group(Right1, Right2, Right3, Right4),
-//Devan do ur thing with the inertial sensor
-PORT1,
-//wheel diameter
+
+//Port number of inertial sensors.
+PORT21,
+
+//Input your wheel diameter. (4" omnis are actually closer to 4.125"):
 2.75,
-//Gear ratio
+
+//External ratio, must be in decimal, in the format of input teeth/output teeth.
+//If your motor has an 84-tooth gear and your wheel has a 60-tooth gear, this value will be 1.4.
+//If the motor drives the wheel directly, this value is 1:
 1,
+
 //Gyro scale, this is what your gyro reads when you spin the robot 360 degrees.
+//For most cases 360 will do fine here, but this scale factor can be very helpful when precision is necessary.
 360,
-//remainder is for odom
+
+/*---------------------------------------------------------------------------*/
+/*                                  PAUSE!                                   */
+/*                                                                           */
+/*  The rest of the drive constructor is for robots using POSITION TRACKING. */
+/*  If you are not using position tracking, leave the rest of the values as  */
+/*  they are.                                                                */
+/*---------------------------------------------------------------------------*/
+
+//If you are using ZERO_TRACKER_ODOM, you ONLY need to adjust the FORWARD TRACKER CENTER DISTANCE.
+
+//FOR HOLONOMIC DRIVES ONLY: Input your drive motors by position. This is only necessary for holonomic drives, otherwise this section can be left alone.
+//LF:      //RF:    
 PORT1,     -PORT2,
+
+//LB:      //RB: 
 PORT3,     -PORT4,
-3,
-2.75,
+
+//If you are using position tracking, this is the Forward Tracker port (the tracker which runs parallel to the direction of the chassis).
+//If this is a rotation sensor, enter it in "PORT1" format, inputting the port below.
+//If this is an encoder, enter the port as an integer. Triport A will be a "1", Triport B will be a "2", etc.
+3, 
+
+//Input the Forward Tracker diameter (reverse it to make the direction switch):
+3.25,
+
+//Input Forward Tracker center distance (a positive distance corresponds to a tracker on the right side of the robot, negative is left.)
 //For a zero tracker tank drive with odom, put the positive distance from the center of the robot to the right side of the drive.
 //This distance is in inches:
--2,
-1,
+6.25, // TODO: RE-MEASURE (HALF WIDTH TO CENTER OF EACH WHEEL)
+
+//Input the Sideways Tracker Port, following the same steps as the Forward Tracker Port:
+PORT13, 
+
+//Sideways tracker diameter (reverse to make the direction switch):
 -2.75,
-5.5
+
+//Sideways tracker center distance (positive distance is behind the center of the robot, negative is in front):
+-7 // TODO: REMEASURE
+
 );
 
 bool auto_started = false;
